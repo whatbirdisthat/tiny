1d43b5c7eabf85f4c90445f5ddadca6d168b6097
wbit <whatbirdisthat@gmail.com>
2016-06-19 02:53:35 +1000

    added styles task

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,19 @@
 var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var sass = require('gulp-sass');
 
+var paths = {
+    pages: ['src/**/*.html']
+};
+
+gulp.task('html', function () {
+    return gulp.src(paths.pages)
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('styles', function () {
+    return gulp.src('src/styles/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
