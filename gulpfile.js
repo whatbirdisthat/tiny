7ae1f789c3333670174b3a4a5b6aee790d5c1b90
wbit <whatbirdisthat@gmail.com>
2016-06-19 07:53:04 +1000

    gulp watch builds first

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
+'use strict';
+
 var gulp = require('gulp');
+var browserify = require('browserify');
+var watchify = require('watchify');
+
 var htmlmin = require('gulp-htmlmin');
 var sass = require('gulp-sass'); // https://github.com/sass/node-sass#options
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var buffer = require('gulp-buffer');
+var gutil = require('gulp-util');
+var assign = require('lodash.assign');
 
 var paths = {
     pages: ['src/**/*.html']
@@ -17,13 +27,38 @@ gulp.task('html', function () {
 gulp.task('styles', function () {
     return gulp.src('src/styles/styles.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle:'compressed', sourceComments: 'map'}).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed', sourceComments: 'map'}).on('error', sass.logError))
         .pipe(concat('styles.css'))
-        .pipe(sourcemaps.write('dist/css/maps'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('watch', function () {
+function handleError(theError) {
+    console.log(theError.toString());
+    this.emit('end');
+}
+
+gulp.task('scripts', function () {
+    var b = browserify({
+        entries: ['./src/scripts/js/main.js'],
+        debug: true,
+        cache: {},
+        packageCache: {},
+        plugin: [watchify]
+    });
+//    return b.bundle().on('error', handleError)
+    return b.bundle().on('error', handleError)
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+        .pipe(uglify()).on('error', gutil.log)
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', ['html', 'styles', 'scripts'], function () {
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
     gulp.watch('src/styles/**/*.scss', ['styles']);
     gulp.watch('src/**/*.html', ['html']);
 });
+
