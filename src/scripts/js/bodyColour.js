3c8c8b5c36131ec73b9fae786e29e88c2385c4d9
wbit <whatbirdisthat@gmail.com>
2016-06-20 15:28:44 +1000

    comment out unnecessary line

--- src/scripts/js/bodyColour.js
+++ src/scripts/js/bodyColour.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 export default function bodyColour(colour) {
-    var theBody = $('body');
+    //var theBody = $('body');
     console.log("BODY-COLOUR: " + colour);
     // theBody.style.backgroundColor = colour;
 }
