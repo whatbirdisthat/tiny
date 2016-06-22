eee6247a39e737f22bd0823a5699e057d315b57b
wbit <whatbirdisthat@gmail.com>
2016-06-22 17:34:58 +1000

    a working ng2 app that doesn't do anything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { RouterLink, RouterOutlet, RouteConfig, ROUTER_DIRECTIVES } from '@angul
     { path: '/about', component: TheAppComponent, name: 'About' }
 ])
 
+
 export class TheAppComponent {
     name1: string;
     name2: string;
--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,8 @@ import {bootstrap}    from '@angular/platform-browser-dynamic';
 
 import {TheAppComponent} from './app.component';
 
+
+
 bootstrap(
     TheAppComponent
 );
